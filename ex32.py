cea95976b5961f34b118b5a2149cbd55192e1313
George Oms <oms@google.com>
2013-08-08 16:27:35 -0700

    progress update

---
+++ ex32.py
@@ -0,0 +1,11 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apple', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print "This is count %d" % number
+
+# same as above
+for fruit in fruits:
+    print "A fruit of type: %s" % fruit
